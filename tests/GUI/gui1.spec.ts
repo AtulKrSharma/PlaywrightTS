d67078e7059a7cdc0ec4090c204e8206e6ec7897
Atul Sharma <atul.sharma@fcc-fac.ca>
2024-06-15 07:33:28 -0400

    updated the config and GUI test cases

---
+++ tests/GUI/gui1.spec.ts
@@ -0,0 +1,35 @@
+import { test, Page, expect } from '@playwright/test';
+
+test.describe('GUI', () => {
+  //this is good
+  test('gui test-1', async ({ page }) => {
+    await page.goto('https://demoblaze.com/');
+    const pageTitle = await page.title();
+    expect(pageTitle).toEqual('STORE');
+  });
+
+  //this is asserting that promise is equal to STORE
+  test('gui test-2', async ({ page }) => {
+    await page.goto('https://demoblaze.com/');
+    const pageTitle = page.title();
+    expect(pageTitle).toEqual('STORE');
+  });
+
+  //this is good
+  test('gui test-3', async ({ page }) => {
+    await page.goto('https://demoblaze.com/');
+    console.log(page.title());
+    await expect(page).toHaveTitle('STORE');
+  });
+});
+
+// Asynchronous Matchers: These matchers return a promise and need await to ensure that the test waits for the assertion to complete. Common examples include:
+
+// expect(page).toHaveTitle()
+// expect(page).toHaveURL()
+// expect(locator).toHaveText()
+// expect(locator).toBeVisible()
+// Synchronous Matchers: These matchers do not return a promise and do not require await. Examples include:
+
+// expect(value).toEqual()
+// expect(array).toContain()
